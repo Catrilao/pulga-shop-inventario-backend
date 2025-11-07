0f2b5007f50a9c9344df1411f9674629ecd4e396
Catrilao <87039539+Catrilao@users.noreply.github.com>
2025-11-07 18:29:37 -0300

    refactor: simplificar manejo de errores

--- src/producto/producto.controller.ts
+++ src/producto/producto.controller.ts
@@ -1,11 +1,4 @@
-import {
-  BadRequestException,
-  Body,
-  Controller,
-  HttpException,
-  Post,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { ProductoService } from './producto.service';
@@ -20,17 +13,6 @@ export class ProductoController {
   @Post()
   @Roles('vendedor')
   async create(@Body() createProductoDto: CreateProductoDto) {
-    try {
-      return await this.productoService.create(createProductoDto);
-    } catch (error) {
-      if (error instanceof HttpException) {
-        throw error;
-      }
-      const message =
-        error && typeof error === 'object' && 'message' in error
-        ? (error as any).message
-        : String(error ?? 'Bad Request');
-      throw new BadRequestException(message)
-    }
+    return await this.productoService.create(createProductoDto);
   }
 }
--- src/producto/producto.service.ts
+++ src/producto/producto.service.ts
@@ -1,35 +1,15 @@
-import {
-  BadRequestException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateProductoDto } from './dto/create-producto.dto';
 import { v4 as uuidv4 } from 'uuid';
 import { ERROR_CODES } from 'src/common/constants/error-codes';
+import { TIENDA_ERROR_CODES } from 'src/tienda/constants/error-codes';
 
 @Injectable()
 export class ProductoService {
   constructor(private prisma: PrismaService) {}
 
   async create(createProductoDto: CreateProductoDto) {
-    if (createProductoDto.stock <= 0) {
-      throw new BadRequestException({
-        message: 'El stock inicial debe ser mayor a 0',
-        error: ERROR_CODES.STOCK_INVALIDO,
-      });
-    }
-
-    if (
-      createProductoDto.precio <= 0 ||
-      !Number.isInteger(createProductoDto.precio)
-    ) {
-      throw new BadRequestException({
-        message: 'El precio debe ser un número entero mayor a 0',
-        error: ERROR_CODES.PRECIO_INVALIDO,
-      });
-    }
-
     const { disponible, id_tienda, ...losDemas } = createProductoDto;
 
     const tiendaExiste = await this.prisma.tienda.findUnique({
@@ -38,7 +18,7 @@ export class ProductoService {
     if (!tiendaExiste) {
       throw new NotFoundException({
         message: 'La tienda no existe',
-        error: ERROR_CODES.TIENDA_NO_EXISTE,
+        error: TIENDA_ERROR_CODES.TIENDA_NO_EXISTE,
       });
     }
 
