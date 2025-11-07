ca72a010c4405ba7185f8472c9f822fc18d4cbda
Catrilao <87039539+Catrilao@users.noreply.github.com>
2025-11-07 17:36:24 -0300

    chore: eliminar archivos no utilizados

--- prisma/migrations/20251018123943_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Tienda" (
-    "id_tienda" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "id_vendedor" INTEGER NOT NULL,
-    "nombre" TEXT NOT NULL,
-    "direccion" TEXT NOT NULL,
-    "telefono" TEXT,
-    "fecha_creacion" DATETIME NOT NULL
-);
